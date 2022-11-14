a022a224f5c081d4af356d89045137e42a7e102c
Blake Moody <32070003+TheSwiginator@users.noreply.github.com>
2022-11-13 19:10:15 -0500

    added rest of the spotify requests from ref site

--- components/spotify.ts
+++ components/spotify.ts
@@ -27,6 +27,8 @@ function _buildUrl(endpoint: string, queryParams: {[key: string]: any} | null = 
                 "seed_genres",
                 "seed_tracks",
                 "type",
+                "additonal_types",
+                "uris"
             ].includes(key)) {
                 queryString += key.concat("=", stringifyList(queryParams[key]));
             } else if ([
@@ -77,6 +79,7 @@ function _buildUrl(endpoint: string, queryParams: {[key: string]: any} | null = 
                 "target_time_signature",
                 "target_valence",
                 "include_external",
+                "position"
             ].includes(key)) {
                 queryString += key.concat("=", value);
             } else if (key == "q") {
@@ -107,7 +110,6 @@ class Spotify {
     code: string | null;
     getAlbum: any;
     getAlbums: any;
-    getPlaylist: any;
     getAlbumTracks: any;
     getUsersSavedAlbums: any;
     saveAlbumsForUser: any;
@@ -151,6 +153,7 @@ class Spotify {
     getAudioFeaturesForTrack: any;
     getAudioAnalysisForTrack: any;
     getRecommendations: any;
+    search: any;
     getCurrentUsersProfile: any;
     getUsersTopItems: any;
     getUsersProfile: any;
@@ -161,6 +164,40 @@ class Spotify {
     unfollow: any;
     isFollowing: any;
     isFollowingPlaylist: any;
+    getPlaylist: any;
+    changePlaylistDetails: any;
+    getPlaylistTracks: any;
+    addTracksToPlaylist: any;
+    updatePlaylistTracks: any;
+    removePlaylistTracks: any;
+    getCurrentUsersPlaylists: any;
+    getUsersPlaylists: any;
+    createPlaylist: any;
+    getFeaturedPlaylists: any;
+    getCategorysPlaylists: any;
+    getPlaylistCoverImage: any;
+    addPlaylistCoverImage: any;
+    getBrowseCategories: any;
+    getBrowseCategory: any;
+    getGenreSeeds: any;
+    getPlaybackState: any;
+    transferPlayback: any;
+    getAvailableDevices: any;
+    getCurrentlyPlaying: any;
+    startResumePlayback: any;
+    pausePlayback: any;
+    skipPlaybackToNext: any;
+    skipPlaybackToPrevious: any;
+    seekPlayback: any;
+    setRepeatMode: any;
+    setPlaybackVolume: any;
+    toggleShuffle: any;
+    getRecentlyPlayed: any;
+    getQueue: any;
+    addToQueue: any;
+    getAvailableMarkets: any;
+
+    
 
 
     constructor(clientId: string, clientSecret: string, redirectURL: string = 'http://localhost:3000/login') {
@@ -170,6 +207,11 @@ class Spotify {
         this.accessToken = null;
         this.refreshToken = null;
         this.code = null;
+        this.initMethods();
+    }
+    
+    initMethods() {
+        // Albums
         this.getAlbum = (id: string, q: QueryParams = null) => this._get(`/albums/${id}`, q);
         this.getAlbums = (q: QueryParams = null) => this._get(`/albums`, q);
         this.getAlbumTracks = (id: string, q: QueryParams = null) => this._get(`/albums/${id}/tracks`, q);
@@ -178,11 +220,13 @@ class Spotify {
         this.removeUsersSavedAlbums = (q: QueryParams = null) => this._delete(`/me/albums`, q, q);
         this.checkUsersSavedAlbums = (q: QueryParams = null) => this._get(`/me/albums/contains`, q);
         this.getNewReleases = (q: QueryParams = null) => this._get(`/browse/new-releases`, q);
+        // Artists
         this.getArtist = (id: string, q: QueryParams = null) => this._get(`/artists/${id}`, q);
         this.getArtists = (q: QueryParams = null) => this._get(`/artists`, q);
         this.getArtistAlbums = (id: string, q: QueryParams = null) => this._get(`/artists/${id}/albums`, q);
         this.getArtistTopTracks = (id: string, q: QueryParams = null) => this._get(`/artists/${id}/top-tracks`, q);
         this.getArtistRelatedArtists = (id: string) => this._get(`/artists/${id}/related-artists`);
+        // Shows
         this.getShow = (id: string, q: QueryParams = null) => this._get(`/shows/${id}`, q);
         this.getShows = (q: QueryParams = null) => this._get(`/shows`, q);
         this.getShowEpisodes = (id: string, q: QueryParams = null) => this._get(`/shows/${id}/episodes`, q);
@@ -190,12 +234,14 @@ class Spotify {
         this.saveShowsForUser = (q: QueryParams = null) => this._put(`/me/shows`, q, q);
         this.removeUsersSavedShows = (q: QueryParams = null) => this._delete(`/me/shows`, q, q);
         this.checkUsersSavedShows = (q: QueryParams = null) => this._get(`/me/shows/contains`, q);
+        // Episodes
         this.getEpisode = (id: string, q: QueryParams = null) => this._get(`/episodes/${id}`, q);
         this.getSeveralEpisodes = (q: QueryParams = null) => this._get(`/episodes`, q);
         this.getUsersSavedEpisodes = (q: QueryParams = null) => this._get(`/me/episodes`, q);
         this.saveEpisodesForUser = (q: QueryParams = null) => this._put(`/me/episodes`, q, q);
         this.removeUsersSavedEpisodes = (q: QueryParams = null) => this._delete(`/me/episodes`, q, q);
         this.checkUsersSavedEpisodes = (q: QueryParams = null) => this._get(`/me/episodes/contains`, q);
+        // Audiobooks
         this.getAudiobook = (id: string, q: QueryParams = null) => this._get(`/audiobooks/${id}`, q);
         this.getAudiobooks = (q: QueryParams = null) => this._get(`/audiobooks`, q);
         this.getAudiobookChapters = (id: string, q: QueryParams = null) => this._get(`/audiobooks/${id}/chapters`, q);
@@ -203,8 +249,10 @@ class Spotify {
         this.saveAudiobooksForUser = (q: QueryParams = null) => this._put(`/me/audiobooks`, q, q);
         this.removeUsersSavedAudiobooks = (q: QueryParams = null) => this._delete(`/me/audiobooks`, q, q);
         this.checkUsersSavedAudiobooks = (q: QueryParams = null) => this._get(`/me/audiobooks/contains`, q);
+        // Chapters
         this.getChapter = (id: string, q: QueryParams = null) => this._get(`/chapters/${id}`, q);
         this.getChapters = (q: QueryParams = null) => this._get(`/chapters`, q);
+        // Tracks
         this.getTrack = (id: string, q: QueryParams = null) => this._get(`/tracks/${id}`, q);
         this.getTracks = (q: QueryParams = null) => this._get(`/tracks`, q);
         this.getUsersSavedTracks = (q: QueryParams = null) => this._get(`/me/tracks`, q);
@@ -215,6 +263,9 @@ class Spotify {
         this.getAudioFeaturesForTracks = (q: QueryParams = null) => this._get(`/audio-features`, q);
         this.getAudioAnalysisForTrack = (id: string) => this._get(`/audio-analysis/${id}`);
         this.getRecommendations = (q: QueryParams = null) => this._get(`/recommendations`, q);
+        // Search
+        this.search = (q: QueryParams = null) => this._get(`/search`, q);
+        // Users
         this.getCurrentUsersProfile = (q: QueryParams = null) => this._get(`/me`, q);
         this.getUsersTopItems = (q: QueryParams = null) => this._get(`/me/top`, q);
         this.getUsersProfile = (id: string, q: QueryParams = null) => this._get(`/users/${id}`, q);
@@ -225,13 +276,46 @@ class Spotify {
         this.unfollow = (q: QueryParams = null) => this._delete(`/me/following`, q, q);
         this.isFollowing = (q: QueryParams = null) => this._get(`/me/following/contains`, q);
         this.isFollowingPlaylist = (id: string, q: QueryParams = null) => this._get(`/playlists/${id}/followers/contains`, q);
-        
-
-
-
+        // Playlists
+        this.getPlaylist = (id: string, q: QueryParams = null) => this._get(`/playlists/${id}`, q);
+        this.changePlaylistDetails = (id: string, q: QueryParams = null) => this._put(`/playlists/${id}`, q, q);
+        this.getPlaylistTracks = (id: string, q: QueryParams = null) => this._get(`/playlists/${id}/tracks`, q);
+        this.addTracksToPlaylist = (id: string, q: QueryParams = null) => this._post(`/playlists/${id}/tracks`, q, q);
+        this.updatePlaylistTracks = (id: string, q: QueryParams = null) => this._put(`/playlists/${id}/tracks`, q, q);
+        this.removePlaylistTracks = (id: string, q: QueryParams = null): any => this._delete(`/playlists/${id}/tracks`, q, q);
+        this.getCurrentUsersPlaylists = (q: QueryParams = null) => this._get(`/me/playlists`, q);
+        this.getUsersPlaylists = (id: string, q: QueryParams = null) => this._get(`/users/${id}/playlists`, q);
+        this.createPlaylist = (id: string, q: QueryParams = null) => this._post(`/users/${id}/playlists`, q, q);
+        this.getFeaturedPlaylists = (q: QueryParams = null) => this._get(`/browse/featured-playlists`, q);
+        this.getCategorysPlaylists = (id: string, q: QueryParams = null) => this._get(`/browse/categories/${id}/playlists`, q);
+        this.getPlaylistCoverImage = (id: string) => this._get(`/playlists/${id}/images`);
+        this.addPlaylistCoverImage = (id: string, q: QueryParams = null) => this._put(`/playlists/${id}/images`, q, q);
+        // Categories
+        this.getBrowseCategories = (q: QueryParams = null) => this._get(`/browse/categories`, q);
+        this.getBrowseCategory = (id: string, q: QueryParams = null) => this._get(`/browse/categories/${id}`, q);
+        // Genres
+        this.getGenreSeeds = () => this._get(`/recommendations/available-genre-seeds`);
+        // Player
+        this.getPlaybackState = (q: QueryParams = null) => this._get(`/me/player`, q);
+        this.transferPlayback = (q: QueryParams = null) => this._put(`/me/player`, q, q);
+        this.getAvailableDevices = (q: QueryParams = null) => this._get(`/me/player/devices`, q);
+        this.getCurrentlyPlaying = (q: QueryParams = null) => this._get(`/me/player/currently-playing`, q);
+        this.startResumePlayback = (q: QueryParams = null) => this._put(`/me/player/play`, q, q);
+        this.pausePlayback = (q: QueryParams = null) => this._put(`/me/player/pause`, q, q);
+        this.skipPlaybackToNext = (q: QueryParams = null) => this._post(`/me/player/next`, q, q);
+        this.skipPlaybackToPrevious = (q: QueryParams = null) => this._post(`/me/player/previous`, q, q);
+        this.seekPlayback = (q: QueryParams = null) => this._put(`/me/player/seek`, q, q);
+        this.setRepeatMode = (q: QueryParams = null) => this._put(`/me/player/repeat`, q, q);
+        this.setPlaybackVolume = (q: QueryParams = null) => this._put(`/me/player/volume`, q, q);
+        this.toggleShuffle = (q: QueryParams = null) => this._put(`/me/player/shuffle`, q, q);
+        this.getRecentlyPlayed = (q: QueryParams = null) => this._get(`/me/player/recently-played`, q);
+        this.getQueue = (q: QueryParams = null) => this._get(`/me/player/queue`, q);
+        this.addToQueue = (q: QueryParams = null) => this._post(`/me/player/queue`, q, q);
+        // Markets
+        this.getAvailableMarkets = () => this._get(`/markets`);
+        // TODO : Start after remove playlist items for checking new query params
 
     }
-    
 
     async getToken(code: string) {
         console.log(code)
@@ -330,6 +414,21 @@ class Spotify {
             this._getRefresh()
             return
         }
+    }
+
+    async _post(endpoint: any, body: BodyParams = null, query: QueryParams = null) {
+        const options = {
+            headers: {'Authorization': `Bearer ${this.accessToken}`, 'Content-Type': 'application/json'},
+        }
+
+        const request = await axios.post(_buildUrl(endpoint, query), body, options);
+
+        if (request.status === 403) {
+            this._getRefresh()
+            return
+        }
+
+        return request.data
     }
 
 }
