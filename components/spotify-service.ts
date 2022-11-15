629f252b56ea06aac569c0eabe26372f90c2f674
Blake Moody <32070003+TheSwiginator@users.noreply.github.com>
2022-11-15 15:59:56 -0500

    specified dependencies in package.json

--- components/spotify-service.ts
+++
@@ -1,7 +0,0 @@
-import SpotifyClient from "./spotify-client";
-import { SPOTIFY_CLIENT_ID , SPOTIFY_SECRET_ID , SPOTIFY_SCOPES , SPOTIFY_REDIRECT_URL} from '../common/constants'
-
-const spotify = new SpotifyClient(SPOTIFY_CLIENT_ID, SPOTIFY_SECRET_ID, SPOTIFY_REDIRECT_URL, SPOTIFY_SCOPES);
-console.log(spotify.authURL);
-
-export default spotify;
